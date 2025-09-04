14bfad0a7275317b625cb794967158303404e1bb
Rtkmak14 <Rtkmak14@gmail.com>
2025-09-04 13:58:50 -0500

    finalized app

--- server.js
+++ server.js
@@ -6,10 +6,19 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
+const applicationsController = require('./controllers/applications.js');
+
 
 const authController = require('./controllers/auth.js');
 
+
+// require our new middleware!
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
 const port = process.env.PORT ? process.env.PORT : '3000';
+
+const path = require('path');
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -20,6 +29,9 @@ mongoose.connection.on('connected', () => {
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 // app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -28,21 +40,23 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => {
-  res.render('index.ejs', {
-    user: req.session.user,
-  });
-});
+app.use(passUserToView);
 
-app.get('/vip-lounge', (req, res) => {
+app.get('/', (req, res) => {
+  // Check if the user is signed in
   if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
+    // Redirect signed-in users to their applications index
+    res.redirect(`/users/${req.session.user._id}/applications`);
   } else {
-    res.send('Sorry, no guests allowed.');
+    // Show the homepage for users who are not signed in
+    res.render('index.ejs');
   }
 });
 
+
 app.use('/auth', authController);
+app.use(isSignedIn);
+app.use('/users/:userId/applications', applicationsController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
